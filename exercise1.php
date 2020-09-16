d6446f3641fbaeb1954f3445e3fb94b9eab8cbc0
Bas Mahieu <bas.mahieu@gmail.com>
2020-09-16 15:34:36 +0200

    adding exercise 1

---
+++ exercise1.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+class Beverage
+{
+    public string $color;
+    public float $price;
+    public string $temperature = 'cold';
+
+    public function __construct(string $color, float $price, string $temperature = 'cold')
+    {
+        $this->color = $color;
+        $this->price = $price;
+        $this->temperature = $temperature;
+    }
+
+    public function getInfo(): string
+    {
+        return "This beverage is $this->temperature and $this->color.";
+    }
+}
+
+$cola = new Beverage('black', 4);
+echo $cola->temperature;
+echo $cola->getInfo();
