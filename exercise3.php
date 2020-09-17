0266bf1f761751433c543cd240bd141f770064de
Bas Mahieu <bas.mahieu@gmail.com>
2020-09-17 12:09:52 +0200

    adding exercise 1

--- exercise3.php
+++ exercise3.php
@@ -68,12 +68,27 @@ class Beer extends Beverage
     {
         return 'Hi, I\'m <b>' . $this->name . '</b> and have an alcohol percentage of <b>' . $this->alcoholpercentage . '%</b>. I\'m <b>' . $this->color . '</b> in color.</br>';
     }
+
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
 }
 
 $duvel = new Beer('blond', 3.50, 'cold', 'duvel', 8.5);
 $guldenDraak = new beer('redish', 4, 'cold', 'Gulden Draak', 10.5);
 
 // Duvel
+echo '<h3>Duvel</h3>';
 echo ($duvel->getInfo());
 $duvel->setColor('light');
 echo ($duvel->getInfo());
@@ -82,9 +97,13 @@ echo ($duvel->getAlcoholPercentage());
 echo '</br>';
 
 // Gulden Draak Test
+echo '<h3>Gulden Draak</h3>';
 echo ($guldenDraak->getInfo());
-$guldenDraak->setColor('ramber');
-$guldenDraak->setTemperature('HOT');
+$guldenDraak->setColor('amber');
+echo ($guldenDraak->getInfo());
+$guldenDraak->setName('New Gulden Dragon');
+$guldenDraak->setColor('green');
+$guldenDraak->setTemperature('hot');
 echo ($guldenDraak->getInfo());
 echo ($guldenDraak->beerInfo());
 echo ($guldenDraak->getAlcoholPercentage());
